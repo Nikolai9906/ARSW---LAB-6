424d13dc476a2ae10fe329220e4560ef13380d8b
Nikolai9906 <nicohhhh1@gmail.com>
2021-03-04 09:46:15 -0500

    Logic Front End in progress"

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,13 @@
+var app = (function(){
+    var bluePrintsList = [{
+        name: "house",
+        numberPoints: 3
+    },{
+        name: "bike",
+        numberPoints: 2
+    },{
+        name: "kryptonite",
+        numberPoints: 2
+    }]
+
+})
