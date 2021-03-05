52643ab086c09c10ee1ae88d1134087f763122d4
Nikolai9906 <nicohhhh1@gmail.com>
2021-03-04 19:40:02 -0500

    Front-End Logic done

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -113,9 +113,13 @@ var apimock = (function () {
 
         getBlueprintsByNameAndAuthor: function(name, author, callback) {
             blueprint = mockdata[author].find(function(blueprint) {
-                return blueprint.name == name
+                return blueprint.name === name
             });
             callback(null, blueprint)
+        },
+
+        getMockdata : () =>{
+            return mockdata
         }
     }
 
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,13 +1,81 @@
 var app = (function(){
-    var bluePrintsList = [{
-        name: "house",
-        numberPoints: 3
-    },{
-        name: "bike",
-        numberPoints: 2
-    },{
-        name: "kryptonite",
-        numberPoints: 2
-    }]
+    var name = "";
+    var blueprints = []
+    var mockdata = apimock.getMockdata()
 
-})
+    getAuthor = function(){
+        name = $("#name").val()
+        $("#authorTitle").text(name +"'s Blueprints")
+        //console.log(name)
+    }
+
+    getBlueprints = function(){
+        getAuthor();
+        //console.log(name)
+        //apimock.getBlueprintsByAuthor(name,(err, res) => {
+                //console.log(mockdata[name][0]);
+                for(let i = 0; i < mockdata[name].length; i++) {
+                  var blueprint = {
+                    name: mockdata[name][i].author,
+                    size: mockdata[name][i].points.length
+                }
+                console.log(blueprint);
+                blueprints.push(blueprint)
+                
+            }
+            console.log(blueprints)
+    }
+    
+
+    changeName = function(newName){
+        name = newName
+    }
+
+    getNameAuthorBlueprints = function() {
+            getAuthor();
+            if (name !== "") {
+                apimock.getBlueprintsByAuthor(name, (req, resp) => {
+                    //console.log(resp)
+                    getData(resp)
+                });
+            } else {
+                alert("Debe ingresar algún nombre, vuelva a intentarlo")
+            }
+    }
+
+    getData = function(resp){
+        $("#tableBlueprints tbody").empty();
+
+        if(resp !== undefined){
+            getAuthor()
+            var data = resp.map((info) => {
+                return {
+                    name: info.name,
+                    points: info.points.length
+                }
+            })
+            data.map((info) => {
+                $("#tableBlueprints > tbody:last").append($("<tr><td>" + info.name +
+                                                            "</td><td>" + info.points.toString() +
+                                                            "</td><td>" + "button Open" +
+                                                            "</td>"))
+            })
+
+            var total = data.reduce((value, {points}) =>
+                value + points , 0
+            )
+            $("#userPoints").text(total)
+        }
+    }
+
+
+    return{
+        getAuthor : getAuthor,
+        getBlueprints : getBlueprints,
+        changeName: changeName,
+        getNameAuthorBlueprints: getNameAuthorBlueprints
+    }
+
+
+
+})();
